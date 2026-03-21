6d8149c16a128d648eff3056af7245f6bf384975
Bhanu Pratap <caballero.bhanu@gmail.com>
2026-03-22 02:23:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 600
-  }
+  build: { chunkSizeWarningLimit: 700 }
 })
